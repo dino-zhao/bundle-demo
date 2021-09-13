97be671b4459af63cfa43395564f58bb96f77246
dino <dino.zhao@outlook.com>
2021-09-13 11:41:58 +0800

    初始化

---
+++ client/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./App";
+import store from "../app/store";
+import { Provider } from "react-redux";
+if (module.hot) {
+  module.hot.accept();
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+const dist = path.resolve(__dirname, "www");
+
+module.exports = {
+  entry: [
+    "react-hot-loader/patch",
+    "./client/index.js",
+    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true",
+  ],
+  devtool: "inline-source-map",
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, dist),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+};
