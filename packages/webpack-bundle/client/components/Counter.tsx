4cee46c34b29ceb179fe01adf7e11ea09dd8bd07
dino <dino.zhao@outlook.com>
2021-09-14 13:42:56 +0800

    save

--- packages/webpack-bundle/client/components/Counter.tsx
+++ packages/webpack-bundle/client/components/Counter.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppSelector, useAppDispatch } from "@redux/store";
 import { decrement, increment } from "@redux/counter/slice";
 import Query from "./Query";
 export default function Counter() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
-
+  const [state, setState] = useState(0);
   return (
     <div>
       <div>
@@ -13,7 +13,7 @@ export default function Counter() {
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
-          Increment1
+          Increment111
         </button>
         <span>{count}</span>
         <button
@@ -22,6 +22,10 @@ export default function Counter() {
         >
           Decrement
         </button>
+      </div>
+      <div>
+        <span onClick={() => setState((c) => c + 1)}>{state}</span>
+        <input type="text" />
       </div>
       {/* <Query /> */}
     </div>
