df0ad18163418fd71cf3188d20ca64138a89e115
dinoZhao <dino.zhao@outlook.com>
2021-09-16 10:36:39 +0800

    hmr

--- packages/webpack-bundle/client/redux/store.ts
+++ packages/webpack-bundle/client/redux/store.ts
@@ -3,19 +3,18 @@ import counterReducer from "./countSlice";
 import { pokemonApi } from "../services/pokemon";
 import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 import { booksSlice, selectBookById } from "./bookSlice";
-
+import rootReducer from "./reducers";
 const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    books: booksSlice.reducer,
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
-  },
+  reducer: rootReducer,
   devTools: true,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
+if (process.env.NODE_ENV !== "production" && module.hot) {
+  module.hot.accept("./reducers", () => store.replaceReducer(rootReducer));
+}
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
