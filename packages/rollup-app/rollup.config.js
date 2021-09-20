9b887b5e678c224c28a7fdfa135ff7708683ca6a
dino <dino.zhao@outlook.com>
2021-09-20 09:13:49 +0800

    处理图片

--- packages/rollup-app/rollup.config.js
+++ packages/rollup-app/rollup.config.js
@@ -3,27 +3,47 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import serve from "rollup-plugin-serve";
-const html = require("@rollup/plugin-html");
+import livereload from "rollup-plugin-livereload";
+import replace from "@rollup/plugin-replace";
+import postcss from "rollup-plugin-postcss";
+import image from "@rollup/plugin-image";
+import copy from "rollup-plugin-copy";
 const isDev = process.env.NODE_ENV === "development";
 let config = {
-  input: "src/index.ts",
+  input: "src/index.tsx",
   output: {
-    // plugins: [terser()],
+    plugins: [terser()],
     dir: "dist",
     format: "es",
   },
   plugins: [
     resolve({
-      extensions: [".js", ".ts"], //相当于webpack的resolve.extensions
+      extensions: [".js", ".ts", ".tsx", ".jpg"], //相当于webpack的resolve.extensions
     }),
     commonjs(),
     babel({ babelHelpers: "bundled", extensions: [".js", ".ts", ".tsx"] }), //用来解析node_modules的模块,和cmj模块，比如lodash
-    html(),
+
+    replace({
+      "process.env.NODE_ENV": JSON.stringify(
+        isDev ? "production" : "development"
+      ),
+      preventAssignment: true,
+    }), //如果不提供，react会报错process is not defined
+    postcss({
+      extensions: [".css"],
+    }),
+    image(),
+    copy({
+      targets: [{ src: "src/images", dest: "dist" }],
+    }),
   ],
 };
 
 if (isDev) {
-  config.plugins.push(serve({ contentBase: ["dist"], port: "10001" }));
+  config.plugins.push(
+    serve({ contentBase: ["dist"], port: "10001" }),
+    livereload({ watch: "dist" })
+  );
 }
 
 export default config;
--- packages/rollup-app/src/index.ts
+++
@@ -1,3 +0,0 @@
-// import log from "rollup-lib";
-import log from "./log";
-log("3344");
