19f47aaef0e43e2edbc5e9d7ee2c713824c3b9d5
dinoZhao <dino.zhao@outlook.com>
2021-09-23 17:47:00 +0800

    添加hook相关lint

--- packages/webpack-app-redux/client/components/Counter.tsx
+++ packages/webpack-app-redux/client/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'antd'
 import midImg from '@/assets/m.jpg'
 import styles from './Counter.module.scss'
@@ -15,6 +15,7 @@ export default function Counter() {
   const { value, loading } = useAppSelector((state) => state.counter)
   const dispatch = useAppDispatch()
   const [state, setState] = useState(0)
+
   return (
     <div>
       <img style={{ display: 'none' }} src={midImg} alt="" />
