4f3563947f86935440a086129c3d233487d865a2
dinoZhao <dino.zhao@outlook.com>
2021-11-04 11:23:29 +0800

    删除不必要的插件

--- packages/module-federation/app2/src/Button.js
+++ packages/module-federation/app2/src/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = () => (
-  <button onClick={() => console.log("另一个蛋糕223")}>App 2 Button</button>
+  <button onClick={() => console.log("另一个蛋糕22")}>App 2 Button</button>
 );
 
 export default Button;
