19c4390d2986015160c35d1e889dea8509f446d3
dino <dino.zhao@outlook.com>
2021-11-22 23:07:20 +0800

    添加普通表格

--- packages/webpack-app-redux/client/components/antd/index.tsx
+++ packages/webpack-app-redux/client/components/antd/index.tsx
@@ -2,6 +2,7 @@ import TabPage, { TabItem } from '@components/common/TabPage'
 import EditableCellTable from './editableCellTable'
 import EditableTrTable from './editableTrTable'
 import VirtualTable from './virtualTable'
+import OrdinaryTable from './ordinaryTable'
 const tabList: TabItem[] = [
   {
     name: '可编辑单元格',
@@ -14,6 +15,10 @@ const tabList: TabItem[] = [
   {
     name: '虚拟表格',
     component: VirtualTable,
+  },
+  {
+    name: '普通表格',
+    component: OrdinaryTable,
   },
 ]
 
