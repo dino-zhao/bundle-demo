e186aff16c70aaa178a28f674498e118e8b74b0d
dino <dino.zhao@outlook.com>
2021-12-03 08:59:11 +0800

    int

--- packages/next-app/pages/index.tsx
+++ packages/next-app/pages/index.tsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import LocaleSwitcher from "../components/locale-switcher";
-
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+const messagesInFrench = {
+  myMessage: "Aujourd'hui, c'est le {ts, date, ::yyyyMMdd}",
+};
 export default function IndexPage(props) {
   const router = useRouter();
   const { locale, locales, defaultLocale } = router;
 
   return (
     <div>
+      <IntlProvider messages={messagesInFrench} locale="fr" defaultLocale="en">
+        <p>
+          <FormattedMessage
+            id="myMessage"
+            defaultMessage="Today is {ts, date, ::yyyyMMdd}"
+            values={{ ts: Date.now() }}
+          />
+          <br />
+          <FormattedNumber value={19} style="currency" currency="EUR" />
+        </p>
+      </IntlProvider>
+
       <h1>Index page</h1>
       <p>Current locale: {locale}</p>
       <p>Default locale: {defaultLocale}</p>
