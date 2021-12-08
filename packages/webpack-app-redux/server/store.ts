89e51e2bb4314759f48ce109e28c3487b993fa38
dino <dino.zhao@outlook.com>
2021-12-08 22:57:59 +0800

    添加uuid

--- packages/webpack-app-redux/server/store.ts
+++ packages/webpack-app-redux/server/store.ts
@@ -1,21 +1,24 @@
 interface Post {
-  id: string
+  id: number
+  name: string
 }
 
 class Store {
   private data: Post[] = [
     {
-      id: 'a',
+      id: 1,
+      name: 'A sample post',
     },
     {
-      id: 'ccc',
+      id: 2,
+      name: 'A post about rtk query',
     },
   ]
   post(v: Post) {
     this.data.push(v)
     return this.data
   }
-  delete(id: string) {
+  delete(id: number) {
     const index = this.data.findIndex((i) => i.id === id)
     if (index > -1) {
       this.data.splice(index, 1)
