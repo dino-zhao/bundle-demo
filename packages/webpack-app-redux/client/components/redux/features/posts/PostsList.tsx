52a4b52bc5942943f645a211779cc1b1003fb5fa
dinoZhao <dino.zhao@outlook.com>
2021-12-09 16:10:49 +0800

    参数变化时修改

--- packages/webpack-app-redux/client/components/redux/features/posts/PostsList.tsx
+++ packages/webpack-app-redux/client/components/redux/features/posts/PostsList.tsx
@@ -3,13 +3,22 @@ import PostDetail from './PostDetail'
 import { useState } from 'react'
 import { Button } from 'antd'
 export default function PostsList() {
-  const { data: posts, isFetching, isLoading } = useGetPostsQuery()
+  const {
+    data: posts,
+    isFetching,
+    isLoading,
+    refetch,
+  } = useGetPostsQuery(undefined, {
+    //参数变化时请求
+    refetchOnMountOrArgChange: true,
+  })
   const [id, setId] = useState(0)
   if (isLoading) return <div>Loading...</div>
   if (!posts) return <div>Missing post!</div>
   function List({ posts }: { posts: Post[] }) {
     return (
       <div>
+        <Button onClick={refetch}> refetch</Button>
         {posts.map((item) => {
           return (
             <div key={item.id}>
