082a0ccde9c8927b0f6774c9c9f4f1f59759eb74
dinoZhao <dino.zhao@outlook.com>
2021-12-16 15:45:16 +0800

    数据库相关配置

--- packages/nest-config-center/src/app.module.ts
+++ packages/nest-config-center/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule, Connection } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
