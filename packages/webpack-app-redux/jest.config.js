386591194467edeb2032055b26698481e84c2a53
dino <dino.zhao@outlook.com>
2021-12-26 20:43:15 +0800

    fix

--- packages/webpack-app-redux/jest.config.js
+++ packages/webpack-app-redux/jest.config.js
@@ -12,6 +12,8 @@ module.exports = {
   moduleNameMapper: {
     '^@components/(.*)$': '<rootDir>/client/components/$1',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
   },
   testEnvironment: 'jsdom',
 }
