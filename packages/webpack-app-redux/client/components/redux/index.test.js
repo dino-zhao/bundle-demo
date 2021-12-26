ce199f9c471405e7cd2783ccdc449ac6f0f623f3
dino <dino.zhao@outlook.com>
2021-12-26 11:44:42 +0800

    修改jest

--- packages/webpack-app-redux/client/components/redux/index.test.js
+++ packages/webpack-app-redux/client/components/redux/index.test.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import ReduxComponent from '.'
 
-test('properly increments and decrements the counter', () => {
-  const { getByText } = render(<ReduxComponent />)
-  const counter = getByText('post列表')
+import Counter from './counter'
 
-  expect(counter.textContent).toEqual('post列表')
+describe('<Counter />', () => {
+  it('properly increments and decrements the counter', () => {
+    const { getByText } = render(<Counter />)
+    const counter = getByText('0')
+    const incrementButton = getByText('+')
+    const decrementButton = getByText('-')
+
+    fireEvent.click(incrementButton)
+    expect(counter.textContent).toEqual('1')
+
+    fireEvent.click(decrementButton)
+    expect(counter.textContent).toEqual('0')
+  })
 })
