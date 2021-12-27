d357ca81a7d5b269af6278dd04ec3b7fcd89e0d1
dinoZhao <dino.zhao@outlook.com>
2021-12-27 17:27:21 +0800

    初始化msw

--- packages/webpack-app-redux/client/components/redux/features/posts/posts.test.js
+++ packages/webpack-app-redux/client/components/redux/features/posts/posts.test.js
@@ -1,35 +1,12 @@
 // __tests__/fetch.test.js
 import React from 'react'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { server, rest } from 'jest-setup'
 import {
   render,
-  fireEvent,
-  waitFor,
   screen,
   waitForElementToBeRemoved,
-  queryByText,
 } from '@components/redux/test-utils'
-// import '@testing-library/jest-dom'
 import PostsList from './PostsList'
-
-const server = setupServer(
-  rest.get('/api/posts', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        code: 0,
-        data: [
-          { id: 1, name: 'A sample post' },
-          { id: 3, name: 'A post about rtk query' },
-        ],
-      })
-    )
-  })
-)
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
 
 test('handles server error', async () => {
   //这个放在后面就不会成功，因为有缓存
@@ -44,6 +21,19 @@ test('handles server error', async () => {
   expect(screen.getByText('Missing post!')).toBeInTheDocument()
 })
 test('loads and displays greeting', async () => {
+  server.use(
+    rest.get('/api/posts', (req, res, ctx) => {
+      return res(
+        ctx.json({
+          code: 0,
+          data: [
+            { id: 1, name: 'A sample post' },
+            { id: 3, name: 'A post about rtk query' },
+          ],
+        })
+      )
+    })
+  )
   render(<PostsList />)
 
   //   fireEvent.click(screen.getByText('refetch'))
