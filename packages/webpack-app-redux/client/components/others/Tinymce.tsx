89a7f061bdd44868a04e56cb65f9094b886952b2
dinoZhao <dino.zhao@outlook.com>
2021-12-28 13:32:52 +0800

    fix

---
+++ packages/webpack-app-redux/client/components/others/Tinymce.tsx
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+
+export default function TinyMce() {
+  const editorRef = useRef(null)
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent())
+    }
+  }
+  const images_upload_handler = async (blob, success, fail) => {
+    const param = new FormData()
+    param.append('img', blob.blob())
+    console.log(blob.blob())
+    // const data = await update_img(param) //update_img是自己定义的上传图片视频方法,需要自行封装，很简单
+    // success(data.url)
+  }
+  return (
+    <>
+      <Editor
+        tinymceScriptSrc="https://statics-websites.oss-cn-hangzhou.aliyuncs.com/tinymce/tinymce.min.js"
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins:
+            'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+          toolbar:
+            'undo redo | formatselect | image | fontsizeselect |' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14pt }',
+          //   images_upload_handler,
+          //   paste_data_images: true,
+          images_upload_handler,
+          fontsize_formats: '8pt 10pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt',
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
+  )
+}
--- packages/webpack-app-redux/client/components/others/index.tsx
+++ packages/webpack-app-redux/client/components/others/index.tsx
@@ -2,11 +2,15 @@ import TabPage, { TabItem } from '@components/common/TabPage'
 import DinoD from './DinoDesign'
 import VisualList from './VisualList'
 import DynamicVisualList from './DynamicVisualList'
-
+import TinyMce from './Tinymce'
 const tabList: TabItem[] = [
   { name: '自定义组件', component: DinoD },
   { name: '虚拟列表', component: VisualList },
   { name: '动态高度虚拟列表', component: DynamicVisualList },
+  {
+    name: '富文本编辑框',
+    component: TinyMce,
+  },
 ]
 
 export default function Others() {
