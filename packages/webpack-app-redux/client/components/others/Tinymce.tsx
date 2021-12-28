53ed513a6fff035b547b17814af0b9c80712fca0
dinoZhao <dino.zhao@outlook.com>
2021-12-28 14:52:19 +0800

    fix

--- packages/webpack-app-redux/client/components/others/Tinymce.tsx
+++ packages/webpack-app-redux/client/components/others/Tinymce.tsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Editor } from '@tinymce/tinymce-react'
+import ReactHtmlParser from 'react-html-parser'
 
 export default function TinyMce() {
   const editorRef = useRef(null)
@@ -15,6 +16,9 @@ export default function TinyMce() {
     // const data = await update_img(param) //update_img是自己定义的上传图片视频方法,需要自行封装，很简单
     // success(data.url)
   }
+  const html = `<p>This is the initial content of the editor.</p>
+<p>&nbsp;</p>
+<p><span style="font-size: 36pt;">sssssss</span></p>`
   return (
     <>
       <Editor
@@ -27,7 +31,7 @@ export default function TinyMce() {
           plugins:
             'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
           toolbar:
-            'undo redo | formatselect | image | fontsizeselect |' +
+            'undo redo | formatselect | image | fontsizeselect | preview |' +
             'bold italic backcolor | alignleft aligncenter ' +
             'alignright alignjustify | bullist numlist outdent indent | ' +
             'removeformat | help',
@@ -40,6 +44,7 @@ export default function TinyMce() {
         }}
       />
       <button onClick={log}>Log editor content</button>
+      <div>{ReactHtmlParser(html)}</div>
     </>
   )
 }
