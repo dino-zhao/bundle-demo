71caa9fd2509f59dd5c65acf21d31d96b85446ed
dino <dino.zhao@outlook.com>
2022-01-28 19:21:12 +0800

    fix

--- packages/webpack-app-redux/cypress/integration/sample_spec.js
+++ packages/webpack-app-redux/cypress/integration/sample_spec.js
@@ -1,5 +1,10 @@
 describe('My First Test', () => {
   it('Does not do much!', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('type')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get(':nth-child(4) > .row > .col-xs-12 > .home-list > :nth-child(1) > ul > :nth-child(1) > a').click();
+    cy.get('#query-btn').click();
+    /* ==== End Cypress Studio ==== */
   })
 })
