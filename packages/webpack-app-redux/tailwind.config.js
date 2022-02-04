1c0d63d55787fd631d033fbf214721cb2964fa2b
dino <dino.zhao@outlook.com>
2022-02-04 13:32:51 +0800

    tailwind

--- packages/webpack-app-redux/tailwind.config.js
+++ packages/webpack-app-redux/tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  purge: ['./client/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./client/**/*.tsx'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
