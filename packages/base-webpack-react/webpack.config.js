2145c751f69235da8409227f6dfe1e4219b8256d
dino <dino.zhao@outlook.com>
2022-07-15 11:31:11 +0800

    fix

--- packages/base-webpack-react/webpack.config.js
+++ packages/base-webpack-react/webpack.config.js
@@ -1,23 +1,28 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
   module: {
     rules: [
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: require.resolve("babel-loader"),
+          },
+        ],
+        include: path.resolve(__dirname, "src"),
       },
     ],
   },
